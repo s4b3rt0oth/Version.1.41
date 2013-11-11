c4218a9d5289f9aaa82cd410aa7ade115e6a66dc
s4b3rt0oth <ajmix2@gmail.com>
2013-11-11 11:01:56 -0500

    Test

--- src/GeoLocation/ShowMap.java
+++ src/GeoLocation/ShowMap.java
@@ -70,8 +70,11 @@ public class ShowMap extends Thread
   public void run()
   {
     try
-    { /* This command has been edited by AJ to work on Mac OSX. I added "open /Application/Google\ Chrome.app/ */
-    	Process process = Runtime.getRuntime().exec("open "/Applications/Google\ Chrome.app/" http://maps.google.com/maps?q=" + this.threadToShow.myGEO_Latitude + "," + this.threadToShow.myGEO_Longitude);
+    {
+    	Process process = Runtime.getRuntime().exec("open -a Google\ Chrome http://maps.google.com/maps?q=" /*+ this.threadToShow.myGEO_Latitude + "," + this.threadToShow.myGEO_Longitude*/);
+    	/*URI uri = URI.create("http://maps.google.com/maps?q=").toURI();
+    	//if this doesn't work try adding .toURI()
+    	desk.browse(uri + this.threadToShow.myGEO_Latitude + "," + this.threadToShow.myGEO_Longitude); */
     }
     catch (Exception e)
     {
